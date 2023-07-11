80b75a166c63b1432def70338e13ccc8702bc50a
excelasaph <e.asaph@alustudent.com>
2023-07-11 10:50:33 -0700

    coding...

--- python-import_modules/103-fast_alphabet.py
+++ python-import_modules/103-fast_alphabet.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
-# 102-magic_calculation.py
-def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
-    from magic_calculation_102 import add, sub
-
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-        return (c)
-
-    else:
-        return (sub(a, b))
+import print_upper
+print(string.ascii_uppercase)
