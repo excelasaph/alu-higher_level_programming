71e5a8552a96c29a1d6fe1b258395672bf16ad76
excelasaph <e.asaph@alustudent.com>
2023-07-11 10:09:14 -0700

    coding...

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import dis
-
-
-def magic_calculation(a, b):
-    return 98 + a ** b
+def magic_calculation(a, b, c):
+    if (a < b):
+        return(c)
+    elif (c > b):
+        return (a + b)
+    return ((a * b) - c)
