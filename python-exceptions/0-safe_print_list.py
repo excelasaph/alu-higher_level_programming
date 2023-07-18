92ecbf089d14279794be689d0d349aa7547fa570
excelasaph <e.asaph@alustudent.com>
2023-07-18 02:00:19 -0700

    coding...

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    if my_list is None:
+        return 0
     count = 0
     try:
-        for i in range(0, x):
-        print("{}".format(my_list[i]), end="")
-        count += 1
-    print()
-    except:
-    print()
-    return count
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        print()
+    except IndexError:
+        print()
+        return count
     return count
