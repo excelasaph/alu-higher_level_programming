c628e61a95b0ead2b62c7d112c90ac8b63e550ad
excelasaph <e.asaph@alustudent.com>
2023-07-18 00:38:13 -0700

    coding...

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+multiple_returns = __import__('8-multiple_returns').multiple_returns
 
-def multiple_returns(sentence):
-    length = len(sentence)
-    if (length == 0):
-        first_char = None
-    else:
-        first_char = sentence[0]
-    return length, first_char
+sentence = "At school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
