e386f76eec4c8a87ad26937a459a73d07fcd507d
excelasaph <e.asaph@alustudent.com>
2023-07-27 23:28:50 -0700

    Python classes

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"Defined a square by size"
+
+
+class Square:
+    """ Square class defines a square by size (private) """
+    def __init__(self, size):
+        """ initializes the object """
+        self.__size = size
