838118287b7ab75e550bb497cab07b7279ed41ee
excelasaph <e.asaph@alustudent.com>
2023-08-17 19:38:03 -0700

    coding...

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-""" a script that lists all State objects from the database hbtn_0e_6_usa
-with the given name
-"""
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine, asc
+"""get a  state."""
+
+
+import sqlalchemy
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
 
-
-def main(argv):
-    """ main function """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-                            argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+if __name__ == "__main__":
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-    try:
-        instance = session.query(State).filter(State.name == argv[4]).one()
-        print(str(instance.id))
-    except:
+    state = session.query(State).filter_by(name=argv[4]).first()
+    if state is not None:
+        print(str(state.id))
+    else:
         print("Not found")
     session.close()
-
-
-if __name__ == '__main__' and len(sys.argv) == 5:
-    main(sys.argv)
