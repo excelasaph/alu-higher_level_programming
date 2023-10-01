56ede0551b10a1e0d226a495e46e80f39efb373b
excelasaph <e.asaph@alustudent.com>
2023-10-01 14:10:10 -0700

    thinking in javascript...

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const { argv } = require('process');
+const intNumber = parseInt(argv[2]);
+if (isNaN(intNumber)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${intNumber}`);
+}
