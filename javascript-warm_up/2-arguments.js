72ed10178dabf8b7ecc9adceec82bb4f19f1e43f
excelasaph <e.asaph@alustudent.com>
2023-10-01 13:19:19 -0700

    thinking in javascript...

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const argv = [5, 6, 8, 9, 3, 4, 7];
-if (argv.length <= 2) {
+const { argv } = require('./process');;
+if (argv.length <= 2 {
   console.log('No argument');
 } else if (argv.length === 3) {
   console.log('Argument found');
