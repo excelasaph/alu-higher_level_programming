b3114f046876911d1d016ae2974af74a44c97e48
excelasaph <e.asaph@alustudent.com>
2023-10-01 14:15:40 -0700

    thinking in javascript...

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let i = 0;
+const arrayStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+while (i < arrayStrings.length) {
+  console.log(arrayStrings[i]);
+  ++i;
+}
