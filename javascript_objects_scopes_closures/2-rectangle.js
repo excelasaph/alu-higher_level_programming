799436a7dbcdcc09ee6cebebe0957b7545b1c3f5
excelasaph <e.asaph@alustudent.com>
2023-10-01 15:03:39 -0700

    thinking in javascript...

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,22 +1,12 @@
 #!/usr/bin/node
-const Rectangle = require('./2-rectangle');
-
-const r1 = new Rectangle(2, 3);
-console.log(r1);
-console.log(r1.width);
-console.log(r1.height);
-
-const r2 = new Rectangle(2, -3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
-
-const r3 = new Rectangle(2);
-console.log(r3);
-console.log(r3.width);
-console.log(r3.height);
-
-const r4 = new Rectangle(2, 0);
-console.log(r4);
-console.log(r4.width);
-console.log(r4.height);
+const Rectangle = class {
+  constructor (w, h) {
+    if (w <= 0 || w === undefined || h <= 0 || h === undefined) {
+      // This Block does nothing
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
+module.exports = Rectangle;
