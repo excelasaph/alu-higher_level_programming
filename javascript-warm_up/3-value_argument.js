cbf9075748cab5a6949b1d3f0fca57aac6ae2fbd
excelasaph <e.asaph@alustudent.com>
2023-10-01 13:36:54 -0700

    thinking in javascript...

---
+++ javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const { argv } = require('process');
+let i = 0;
+let elementCount = 0;
+while (argv[i]) {
+  elementCount++;
+  i++;
+}
+if (elementCount <= 2) {
+  console.log('No argument');
+} else {
+  console.log(argv[2]);
+}
